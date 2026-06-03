7ae150559c5a63d995113815d9c77b787c8ef76b
ex_mazy16 <1657642978@qq.com>
2026-06-03 16:30:19 +0800

    4
    4

--- design/build.gradle.kts
+++ design/build.gradle.kts
@@ -4,6 +4,17 @@ plugins {
     id("com.android.library")
 }
 
+kotlin {
+    compilerOptions {
+        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
+    }
+}
+
+kapt {
+    correctErrorTypes = true
+    useBuildCache = false
+}
+
 dependencies {
     implementation(project(":common"))
     implementation(project(":core"))
