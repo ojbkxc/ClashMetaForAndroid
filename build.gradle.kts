15f84a3dcb375fb2a4e57c080cf0dc0017c0a834
GitHub Action <action@github.com>
2026-06-15 12:39:48 +0000

    Bump version to 2.0.43 (200043)

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2.0.42"
-            versionCode = 200042
+            versionName = "2.0.43"
+            versionCode = 200043
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
